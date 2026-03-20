e1f8e84b746539950c520ed062cc385f25729fb6
Stark-Priver <priverdeprince@yahoo.com>
2026-03-20 22:55:08 +0300

    update: isar_provider.dart [2026-03-20 22:55:08]

--- pulse/core/providers/isar_provider.dart
+++ pulse/core/providers/isar_provider.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isar/isar.dart';
-import '../database/models/student_record.dart';
-import '../database/models/attendance_log.dart';
-import '../database/models/sync_queue_item.dart';
 import '../database/repository/isar_repository.dart';
 
 final isarProvider = Provider<Isar>((ref) => throw UnimplementedError());
