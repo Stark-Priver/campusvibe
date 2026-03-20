aac61fb8fdce09ce85040d49a6d6d9131890e710
Stark-Priver <priverdeprince@yahoo.com>
2026-03-20 22:31:06 +0300

    update: main.dart [2026-03-20 22:31:06]

--- pulse/lib/main.dart
+++ pulse/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../core/theme/app_theme.dart';
-import '../features/splash/splash_screen.dart';
-import '../features/dashboard/dashboard_screen.dart';
-import '../features/scan/scan_screen.dart';
-import '../features/result/result_screen.dart';
-import '../features/admin/admin_panel_screen.dart';
-import '../features/logs/attendance_logs_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'features/splash/splash_screen.dart';
+import 'features/dashboard/dashboard_screen.dart';
+import 'features/scan/scan_screen.dart';
+import 'features/result/result_screen.dart';
+import 'features/admin/admin_panel_screen.dart';
+import 'features/logs/attendance_logs_screen.dart';
 
 void main() {
   runApp(const PulseApp());
