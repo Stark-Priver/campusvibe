bbe7658796f87295dfdbe46da799c9f7b2a9d413
Stark-Priver <priverdeprince@yahoo.com>
2026-03-20 22:32:05 +0300

    update: scan_screen.dart [2026-03-20 22:32:05]

--- pulse/features/scan/scan_screen.dart
+++ pulse/features/scan/scan_screen.dart
@@ -109,9 +109,7 @@ class _AnimatedScanFrameState extends State<_AnimatedScanFrame>
             border: Border.all(color: const Color(0xFFFFC107), width: 4),
             boxShadow: [
               BoxShadow(
-                color: const Color(
-                  0xFFFFC107,
-                ).withOpacity(_glowAnim.value * 0.25),
+                color: const Color(0xFFFFC107).withAlpha(((_glowAnim.value * 0.25) * 255).round()),
                 blurRadius: 32,
                 spreadRadius: 2,
               ),
